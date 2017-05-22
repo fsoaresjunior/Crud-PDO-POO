fa8ead82e218728a772d40ab86b83f443c3e10c7
Jholdy Damasceno <jholdydamasceno@gmail.com>
2017-05-22 06:24:36 -0300

    Melhorando o index.

--- index.php
+++ index.php
@@ -15,41 +15,22 @@ if (isset($_SESSION['logado']))
 
   $cargo = new Cargos();
 
-  $parans = array('usuario' => $usuario,
-                  'cargo' => $cargo,
-                  'logar' => $logar
-                 );
+  $parans = array( 'usuario' => $usuario, 'cargo' => $cargo, 'logar' => $logar );
 
   include "view/layout/menu.php";
 
-  if (isset($_POST['acao']))
-  {
+  isset($_POST['acao']) ? Post::routes($_POST['acao'], $parans) : '' ;
 
-    Post::routes($_POST['acao'], $parans);
-
-  }
-
-//crud de cargos e usuarios
-
-  if (isset($_GET['acao']))
-  {
-
-    Get::routes($_GET['acao'], $parans);
-
-  }
+  isset($_GET['acao']) ? Get::routes($_GET['acao'], $parans) : '' ;
 
 }
 else
 {
 
-    include "view/login/index.php";
+  include "view/login/index.php";
 
-    if (isset($_POST['acao']))
-    {
-
-      Post::routes($_POST['acao'], array('logar' => $logar));
-
-    }
+  isset($_POST['acao']) ? Post::routes($_POST['acao'], array('logar' => $logar)) : '';
 
 }
-  include "view/layout/footer.php";
+
+include "view/layout/footer.php";
